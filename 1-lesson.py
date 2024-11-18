962b3f92b975b331b73ee5005a858db6fdc4a9ac
Rakhmatullayev-Azizbek <azizbek.rahmatillayev.uz@gmail.com>
2024-11-18 09:54:55 +0500

    yangi faylar qoshildi
    boshlangich darslar yuklandi

---
+++ 1-lesson.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 09:30:55 2024
+
+@author: user
+"""
+
+
+
+ismlar=['Abror','Sarvar','Rozimuhammad']
+print('Salom '+ismlar[0]+' bugun choyxona bormi?')
+print(ismlar[1]+' choyxonaga boramzi?')
---
+++ temp.py
@@ -0,0 +1,6 @@
+login=['azizbek','sarvar','sarah','jack']
+log=input('Loginingizni kiriting:')
+if log.lower() in login:
+    print('Bu login band, boshqasini kiriting')
+else:
+    print('Xush kelibsiz!')
