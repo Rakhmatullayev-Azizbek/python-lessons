fd4e19542ee9c8a640b7575e366769ac5333e51f
Rakhmatullayev-Azizbek <azizbek.rahmatillayev.uz@gmail.com>
2024-11-18 10:03:02 +0500

    1dars
    yangilandi

--- 1-lesson.py
+++ 1-lesson.py
@@ -7,6 +7,6 @@ Created on Mon Nov 18 09:30:55 2024
 
 
 
-ismlar=['Abror','Sarvar','Rozimuhammad']
+r=['Abror','Sarvar','Rozimuhammad']
 print('Salom '+ismlar[0]+' bugun choyxona bormi?')
 print(ismlar[1]+' choyxonaga boramzi?')
